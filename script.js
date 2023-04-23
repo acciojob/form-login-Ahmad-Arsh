411d034862cec76b0feed82b414e46ddbef5d257
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-23 11:05:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	 let firstName = document.querySelector('[name="firstname"]').value;
+    let lastName = document.querySelector('[name="lastname"]').value;
+    alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
 
 }
