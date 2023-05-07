92b144de4a4a207ccda6623b769361b2ed6124cf
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-05-07 12:29:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-function getFormvalue() {
-  const firstName = document.getElementById('fname').value;
-  const lastName = document.getElementById('lname').value;
-  alert(`First name: ${firstName}\nLast name: ${lastName}`);
-}
-document.getElementById('myForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  getFormvalue();
-});
+   function getFormvalue() {
+        let firstName = document.querySelector('[name="firstname"]').value;
+        let lastName = document.querySelector('[name="lastname"]').value;
+        alert(`${firstName} ${lastName}`);
+    }
